b26eb23a7a04a7c44fe893ef130f054fbe3a21b7
rsevil <rsevil@e84052d8-b9b0-0245-9862-b72e0d739b76>
2008-12-15 19:37:42 +0000

    App para recibir en una pagina datos por post y enviar un mail.

---
+++ trunk/WitMailSender/src/WitMailSender.SenderPage/App_Data/MailVariable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace WitMailSender.SenderPage
+{
+    public class MailVariable : ConfigurationElement
+    {
+        [ConfigurationProperty("key", IsRequired = true)]
+        public string Key{
+            get
+            {
+                return (string)this["key"];
+            }
+        }
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string Value
+        {
+            get
+            {
+                return (string)this["value"];
+            }
+        }
+    }
+}
