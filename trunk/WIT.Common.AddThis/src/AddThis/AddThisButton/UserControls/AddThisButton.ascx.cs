47f46c084fdaf6738113cd22d3aa0a78a23469ba
rsevil <rsevil@e84052d8-b9b0-0245-9862-b72e0d739b76>
2009-07-20 15:14:16 +0000

    [WMB-2083]  Agregar los botones de AddThis.com a la pagina de ingreso de una reunion. UserControl del botón para compartir links por redes sociales.

---
+++ trunk/WIT.Common.AddThis/src/AddThis/AddThisButton/UserControls/AddThisButton.ascx.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+
+namespace WIT.Common.AddThisButton.UserControls
+{
+    public partial class AddThisButton : System.Web.UI.UserControl
+    {
+        #region Properties
+        /// <summary>
+        /// URL to share, by default if its not setted will use current location.
+        /// </summary>
+        public string URL
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".URL"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".URL", value);
+            }
+        }
+
+        /// <summary>
+        /// Title of shared object
+        /// </summary>
+        public string Title
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".Title"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".Title", value);
+            }
+        }
+
+        /// <summary>
+        /// For forcing the menu to use a particular language, specified via ISO code. 
+        /// For example, setting this to "sv" would show the menu in Swedish. Note: Regardless of the 
+        /// number of times it's specified, only one language is supported per page.
+        /// </summary>
+        public string SelectedLanguage
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".SelectedLanguage"];
+                }
+                catch
+                {
+                    return WellKnownKeys.ui_language_en;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".SelectedLanguage", value);
+            }
+        }
+
+        /// <summary>
+        /// The text of the button that shows the other sevices in the pop up window.
+        /// </summary>
+        public string TextMore
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".TextMore"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".TextMore", value);
+            }
+        }
+
+        /// <summary>
+        /// The quick services items to show in the html.
+        /// </summary>
+        public string MenuServices
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".MenuServices"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".MenuServices", value);
+            }
+        }
+
+        /// <summary>
+        /// Services to use in the expanded menu. Useful if very few services are desired -- specifying a long list via 
+        /// services_exclude could be tiresome, and wouldn't catch a new service added later. 
+        /// For example, setting this to 'bebo,misterwong,netvibes' would result in only those three services 
+        /// appearing in the expanded menu. Always global.
+        /// </summary>
+        public string IncludeMoreServices
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".IncludeMoreServices"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".IncludeMoreServices", value);
+            }
+        }
+
+        /// <summary>
+        /// Services to exclude from all menus. For example, setting this to 'facebook,myspace' would hide Facebook 
+        /// and MySpace on all our menus. Always global.
+        /// </summary>
+        public string ExcludeMoreServices
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".ExcludeMoreServices"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".ExcludeMoreServices", value);
+            }
+        }
+
+        /// <summary>
+        /// Separator template, can be html or simple text.
+        /// </summary>
+        public string Separator
+        {
+            get
+            {
+                try
+                {
+                    return (string)ViewState[this.UniqueID + ".Separator"];
+                }
+                catch
+                {
+                    return string.Empty;
+                }
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".Separator", value);
+            }
+        }
+
+        /// <summary>
+        /// The version of the used AddThisButtonAPI.
+        /// Default: http://s7.addthis.com/js/250/addthis_widget.js?pub=xa-4a60bb071de35881
+        /// </summary>
+        public string AddThisButtonAPI
+        {
+            get
+            {
+                string addThisButtonAPI = (string)ViewState[this.UniqueID + ".AddThisButtonAPI"];
+                return (!string.IsNullOrEmpty(addThisButtonAPI)) ? addThisButtonAPI : "http://s7.addthis.com/js/250/addthis_widget.js?pub=xa-4a60bb071de35881";
+            }
+            set
+            {
+                ViewState.Add(this.UniqueID + ".AddThisButtonAPI", value);
+            }
+        }
+
+        private bool CanBuildConfigurationAndSharingOptions { get; set; }
+        #endregion
+
+        #region Methods
+        private void BuildConfigurationAndSharingOptions()
+        {
+            if (!this.Page.ClientScript.IsClientScriptBlockRegistered(this.Page.GetType(), "AddThisButtonConfigurationScript"))
+            {
+                StringBuilder script = new StringBuilder();
+                script.Append("<script type='text/javascript' language='javascript'>");
+                script.Append("var addthis_config=");
+                script.Append("{");
+                script.Append(!string.IsNullOrEmpty(IncludeMoreServices) ? "services_expanded: '" + IncludeMoreServices + "'," : string.Empty);
+                script.Append(!string.IsNullOrEmpty(ExcludeMoreServices) ? "services_exclude: '" + ExcludeMoreServices + "'," : string.Empty);
+                script.Append("};");
+                script.Append("var addthis_share={};");
+                script.Append("</script>");
+
+                this.Page.ClientScript.RegisterClientScriptBlock(this.Page.GetType(), "AddThisButtonConfigurationScript", script.ToString());
+            }
+        }
+        private void RegisterAddThisButtonAPI()
+        {
+            if (!this.Page.ClientScript.IsClientScriptBlockRegistered(this.Page.GetType(), "AddThisButtonAPI"))
+            {
+                StringBuilder script = new StringBuilder();
+                script.Append("<script type='text/javascript' src='" + AddThisButtonAPI +"'></script>");
+                this.Page.ClientScript.RegisterClientScriptBlock(this.Page.GetType(), "AddThisButtonAPI", script.ToString());
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BuildConfigurationAndSharingOptions();
+            RegisterAddThisButtonAPI();
+        }
+
+        protected string BuildMenuServices()
+        {
+            if (!string.IsNullOrEmpty(MenuServices) && MenuServices.Contains(','))
+            {
+                string htmlResult = string.Empty;
+                string[] menuServices = MenuServices.Split(',');
+                foreach (string service in menuServices)
+                {
+                    htmlResult += "<a class='addthis_button_" + service + "'></a>";
+                }
+                return htmlResult;
+            }
+            return string.Empty;
+        }
+        #endregion
+    }
+}
---
+++ trunk/WIT.Common.AddThis/src/AddThis/AddThisButton/WellKnownKeys/WellKnownKeys.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WIT.Common.AddThisButton
+{
+    public class WellKnownKeys
+    {
+        #region UI_Languages
+        /// <summary>
+        /// If you do not set the language the menu will change language automatically based on the user's 
+        /// browser language setting. The possible two-letter codes are listed below.
+        /// </summary>
+        public static readonly string ui_language_en = "en"; //English
+        public static readonly string ui_language_zh = "zh"; //Chinese
+        public static readonly string ui_language_da = "da"; //Danish
+        public static readonly string ui_language_nl = "nl"; //Dutch
+        public static readonly string ui_language_fi = "fi"; //Finnish
+        public static readonly string ui_language_fr = "fr"; //French
+        public static readonly string ui_language_de = "de"; //German
+        public static readonly string ui_language_he = "he"; //Hebrew
+        public static readonly string ui_language_it = "it"; //Italian
+        public static readonly string ui_language_ja = "ja"; //Japanese
+        public static readonly string ui_language_ko = "ko"; //Korean
+        public static readonly string ui_language_no = "no"; //Norwegian
+        public static readonly string ui_language_pl = "pl"; //Polish
+        public static readonly string ui_language_pt = "pt"; //Portuguese
+        public static readonly string ui_language_ru = "ru"; //Russian
+        public static readonly string ui_language_sl = "sl"; //Slovenian
+        public static readonly string ui_language_es = "es"; //Spanish
+        public static readonly string ui_language_sv = "sv"; //Swedish
+        public static readonly string ui_language_tr = "tr"; //Turkish
+        public static readonly string ui_language_cy = "cy"; //Welsh
+        #endregion
+
+        #region UI_Services
+        /// <summary>
+        /// For use in service_* configuration options.
+        /// </summary>
+        public static readonly string ui_service_aim = "aim";//AIM                       
+        public static readonly string ui_service_amazonwishlist = "amazonwishlist";//Amazon's Wish List
+        public static readonly string ui_service_ask = "ask";//Ask.com                       
+        public static readonly string ui_service_backflip = "backflip";//Backflip; a social bookmarking tool        
+        public static readonly string ui_service_ballhype = "ballhype";//Ballhype; a social sports news aggregator           
+        public static readonly string ui_service_bebo = "bebo";//Bebo; a social network                
+        public static readonly string ui_service_blogger = "blogger";//Blogger; a blogging platform            
+        public static readonly string ui_service_blogmarks = "blogmarks";//Blogmarks; a social bookmarking tool            
+        public static readonly string ui_service_buzz = "buzz";//Yahoo! Buzz; a social news aggregator
+        public static readonly string ui_service_delicious = "delicious";//Backflip; a social bookmarking tool
+        public static readonly string ui_service_digg = "digg";//Digg; a social news aggregator
+        public static readonly string ui_service_diigo = "diigo";//Diigo; a social bookmarking and annotation tool
+        public static readonly string ui_service_email = "email";//The AddThis email interface
+        public static readonly string ui_service_facebook = "facebook";//Facebook; a social network
+        public static readonly string ui_service_fark = "fark";//Fark; a news aggregator
+        public static readonly string ui_service_faves = "faves";//Faves; a social bookmarking tool
+        public static readonly string ui_service_favorites = "favorites";//Adds the page to the browser's bookmarks
+        public static readonly string ui_service_friendfeed = "friendfeed";//Friendfeed; a micro-blogging platform
+        public static readonly string ui_service_google = "google";//Google Bookmarks; a bookmarking tool
+        public static readonly string ui_service_kaboodle = "kaboodle";//Kaboodle; a social shopping tool
+        public static readonly string ui_service_kirtsy = "kirtsy";//Kirtsy; a social news aggregator
+        public static readonly string ui_service_linkagogo = "linkagogo";//LinkaGoGo; a social bookmarking tool
+        public static readonly string ui_service_linkedin = "linkedin";//LinkedIn; a social network
+        public static readonly string ui_service_live = "live";//Microsoft Live; a media sharing tool
+        public static readonly string ui_service_misterwong = "misterwong";//Mister Wong; a social bookmarking tool
+        public static readonly string ui_service_mixx = "mixx";//Mixx; a social bookmarking tool
+        public static readonly string ui_service_multiply = "multiply";//Multiply; a media sharing tool
+        public static readonly string ui_service_myaol = "myaol";//MyAOL; a start page
+        public static readonly string ui_service_myspace = "myspace";//MySpace; a social network
+        public static readonly string ui_service_netvibes = "netvibes";//Netvibes; a start page
+        public static readonly string ui_service_netvouz = "netvouz";//Netvouz; a social bookmarking tool
+        public static readonly string ui_service_newsvine = "newsvine";//Newsvine; a social news aggregator
+        public static readonly string ui_service_plaxo = "plaxo";//Plaxo; a social network
+        public static readonly string ui_service_propeller = "propeller";//Propeller; a social news aggregator
+        public static readonly string ui_service_print = "print";//Prints the page
+        public static readonly string ui_service_reddit = "reddit";//Reddit; a social news aggregator
+        public static readonly string ui_service_simpy = "simpy";//Simpy; a social bookmarking tool
+        public static readonly string ui_service_slashdot = "slashdot";//Slashdot; a social technology news aggregator
+        public static readonly string ui_service_spurl = "spurl";//Spurl; a social bookmarking tool
+        public static readonly string ui_service_stumbleupon = "stumbleupon";//StumbleUpon; a content discovery tool
+        public static readonly string ui_service_stylehive = "stylehive";//Stylehive; a fashion-focused social bookmarking tool
+        public static readonly string ui_service_technorati = "technorati";//Technorati; a blog indexing and tracking tool
+        public static readonly string ui_service_thisnext = "thisnext";//ThisNext; a social shopping tool
+        public static readonly string ui_service_tipd = "tipd";//Tip'd; a financial news aggregator
+        public static readonly string ui_service_twitter = "twitter";//Twitter; a micro-blogging platform
+        public static readonly string ui_service_tumblr = "tumblr";//Tumblr; a blogging platform
+        public static readonly string ui_service_typepad = "typepad";//TypePad; a blogging platform
+        public static readonly string ui_service_wordpress = "wordpress";//WordPress; a blogging platform
+        public static readonly string ui_service_yahoobkm = "yahoobkm";//Yahoo! Bookmarks; a bookmarking tool
+        public static readonly string ui_service_yardbarker = "yardbarker";//Yardbarker; a social sports news aggregator
+        #endregion
+    }
+}
