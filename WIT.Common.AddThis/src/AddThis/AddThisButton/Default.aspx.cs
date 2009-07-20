7473a747b75325122fcd5f367087f4f1abd039f4
rsevil <rsevil@e84052d8-b9b0-0245-9862-b72e0d739b76>
2009-07-20 15:14:16 +0000

    [WMB-2083]  Agregar los botones de AddThis.com a la pagina de ingreso de una reunion. UserControl del botón para compartir links por redes sociales.

---
+++ WIT.Common.AddThis/src/AddThis/AddThisButton/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WIT.Common.AddThisButton.UserControls;
+using WIT.Common.AddThisButton;
+
+namespace AddThisButton
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AddThisButton1.IncludeMoreServices = AddThisButton2.IncludeMoreServices =
+                WellKnownKeys.ui_service_facebook + "," +
+                WellKnownKeys.ui_service_email + "," +
+                WellKnownKeys.ui_service_favorites + "," +
+                WellKnownKeys.ui_service_twitter;
+
+            AddThisButton1.MenuServices = AddThisButton2.MenuServices =
+                WellKnownKeys.ui_service_facebook + "," +
+                WellKnownKeys.ui_service_email + "," +
+                WellKnownKeys.ui_service_favorites + "," +
+                WellKnownKeys.ui_service_twitter;
+        }
+    }
+}
