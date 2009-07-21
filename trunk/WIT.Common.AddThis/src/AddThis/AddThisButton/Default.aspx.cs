1e07e120b74a00eef9331e0e5360d0c3fc05c0c9
rsevil <rsevil@e84052d8-b9b0-0245-9862-b72e0d739b76>
2009-07-21 16:04:53 +0000

    [WMB-2083]  Agregar los botones de AddThis.com a la pagina de ingreso de una reunion. Ahora el control es un CustomServerControl.

--- trunk/WIT.Common.AddThis/src/AddThis/AddThisButton/Default.aspx.cs
+++ trunk/WIT.Common.AddThis/src/AddThis/AddThisButton/Default.aspx.cs
@@ -4,26 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using WIT.Common.AddThisButton.UserControls;
-using WIT.Common.AddThisButton;
 
-namespace AddThisButton
+namespace WIT.Common.AddThis
 {
     public partial class _Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            AddThisButton1.IncludeMoreServices = AddThisButton2.IncludeMoreServices =
+            AddThisButton1.MenuServices =
                 WellKnownKeys.ui_service_facebook + "," +
                 WellKnownKeys.ui_service_email + "," +
-                WellKnownKeys.ui_service_favorites + "," +
-                WellKnownKeys.ui_service_twitter;
+                WellKnownKeys.ui_service_twitter + "," +
+                WellKnownKeys.ui_service_favorites + ",";
 
-            AddThisButton1.MenuServices = AddThisButton2.MenuServices =
+            AddThisButton2.MenuServices =
                 WellKnownKeys.ui_service_facebook + "," +
                 WellKnownKeys.ui_service_email + "," +
-                WellKnownKeys.ui_service_favorites + "," +
-                WellKnownKeys.ui_service_twitter;
+                WellKnownKeys.ui_service_twitter + "," +
+                WellKnownKeys.ui_service_favorites + ",";
         }
     }
 }
--- trunk/WIT.Common.AddThis/src/AddThis/AddThisButton/WellKnownKeys/WellKnownKeys.cs
+++ trunk/WIT.Common.AddThis/src/AddThis/AddThisButton/WellKnownKeys/WellKnownKeys.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace WIT.Common.AddThisButton
+﻿namespace WIT.Common.AddThis
 {
     public class WellKnownKeys
     {
@@ -38,56 +33,57 @@ namespace WIT.Common.AddThisButton
         /// <summary>
         /// For use in service_* configuration options.
         /// </summary>
-        public static readonly string ui_service_aim = "aim";//AIM                       
-        public static readonly string ui_service_amazonwishlist = "amazonwishlist";//Amazon's Wish List
-        public static readonly string ui_service_ask = "ask";//Ask.com                       
-        public static readonly string ui_service_backflip = "backflip";//Backflip; a social bookmarking tool        
-        public static readonly string ui_service_ballhype = "ballhype";//Ballhype; a social sports news aggregator           
-        public static readonly string ui_service_bebo = "bebo";//Bebo; a social network                
-        public static readonly string ui_service_blogger = "blogger";//Blogger; a blogging platform            
-        public static readonly string ui_service_blogmarks = "blogmarks";//Blogmarks; a social bookmarking tool            
-        public static readonly string ui_service_buzz = "buzz";//Yahoo! Buzz; a social news aggregator
-        public static readonly string ui_service_delicious = "delicious";//Backflip; a social bookmarking tool
-        public static readonly string ui_service_digg = "digg";//Digg; a social news aggregator
-        public static readonly string ui_service_diigo = "diigo";//Diigo; a social bookmarking and annotation tool
-        public static readonly string ui_service_email = "email";//The AddThis email interface
-        public static readonly string ui_service_facebook = "facebook";//Facebook; a social network
-        public static readonly string ui_service_fark = "fark";//Fark; a news aggregator
-        public static readonly string ui_service_faves = "faves";//Faves; a social bookmarking tool
-        public static readonly string ui_service_favorites = "favorites";//Adds the page to the browser's bookmarks
-        public static readonly string ui_service_friendfeed = "friendfeed";//Friendfeed; a micro-blogging platform
-        public static readonly string ui_service_google = "google";//Google Bookmarks; a bookmarking tool
-        public static readonly string ui_service_kaboodle = "kaboodle";//Kaboodle; a social shopping tool
-        public static readonly string ui_service_kirtsy = "kirtsy";//Kirtsy; a social news aggregator
-        public static readonly string ui_service_linkagogo = "linkagogo";//LinkaGoGo; a social bookmarking tool
-        public static readonly string ui_service_linkedin = "linkedin";//LinkedIn; a social network
-        public static readonly string ui_service_live = "live";//Microsoft Live; a media sharing tool
-        public static readonly string ui_service_misterwong = "misterwong";//Mister Wong; a social bookmarking tool
-        public static readonly string ui_service_mixx = "mixx";//Mixx; a social bookmarking tool
-        public static readonly string ui_service_multiply = "multiply";//Multiply; a media sharing tool
-        public static readonly string ui_service_myaol = "myaol";//MyAOL; a start page
-        public static readonly string ui_service_myspace = "myspace";//MySpace; a social network
-        public static readonly string ui_service_netvibes = "netvibes";//Netvibes; a start page
-        public static readonly string ui_service_netvouz = "netvouz";//Netvouz; a social bookmarking tool
-        public static readonly string ui_service_newsvine = "newsvine";//Newsvine; a social news aggregator
-        public static readonly string ui_service_plaxo = "plaxo";//Plaxo; a social network
-        public static readonly string ui_service_propeller = "propeller";//Propeller; a social news aggregator
-        public static readonly string ui_service_print = "print";//Prints the page
-        public static readonly string ui_service_reddit = "reddit";//Reddit; a social news aggregator
-        public static readonly string ui_service_simpy = "simpy";//Simpy; a social bookmarking tool
-        public static readonly string ui_service_slashdot = "slashdot";//Slashdot; a social technology news aggregator
-        public static readonly string ui_service_spurl = "spurl";//Spurl; a social bookmarking tool
-        public static readonly string ui_service_stumbleupon = "stumbleupon";//StumbleUpon; a content discovery tool
-        public static readonly string ui_service_stylehive = "stylehive";//Stylehive; a fashion-focused social bookmarking tool
-        public static readonly string ui_service_technorati = "technorati";//Technorati; a blog indexing and tracking tool
-        public static readonly string ui_service_thisnext = "thisnext";//ThisNext; a social shopping tool
-        public static readonly string ui_service_tipd = "tipd";//Tip'd; a financial news aggregator
-        public static readonly string ui_service_twitter = "twitter";//Twitter; a micro-blogging platform
-        public static readonly string ui_service_tumblr = "tumblr";//Tumblr; a blogging platform
-        public static readonly string ui_service_typepad = "typepad";//TypePad; a blogging platform
-        public static readonly string ui_service_wordpress = "wordpress";//WordPress; a blogging platform
-        public static readonly string ui_service_yahoobkm = "yahoobkm";//Yahoo! Bookmarks; a bookmarking tool
-        public static readonly string ui_service_yardbarker = "yardbarker";//Yardbarker; a social sports news aggregator
+        public static readonly string ui_service_aim = "aim";                       //AIM                       
+        public static readonly string ui_service_amazonwishlist = "amazonwishlist"; //Amazon's Wish List
+        public static readonly string ui_service_ask = "ask";                       //Ask.com                       
+        public static readonly string ui_service_backflip = "backflip";             //Backflip; a social bookmarking tool        
+        public static readonly string ui_service_ballhype = "ballhype";             //Ballhype; a social sports news aggregator           
+        public static readonly string ui_service_bebo = "bebo";                     //Bebo; a social network                
+        public static readonly string ui_service_blogger = "blogger";               //Blogger; a blogging platform            
+        public static readonly string ui_service_blogmarks = "blogmarks";           //Blogmarks; a social bookmarking tool            
+        public static readonly string ui_service_buzz = "buzz";                     //Yahoo! Buzz; a social news aggregator
+        public static readonly string ui_service_delicious = "delicious";           //Backflip; a social bookmarking tool
+        public static readonly string ui_service_digg = "digg";                     //Digg; a social news aggregator
+        public static readonly string ui_service_diigo = "diigo";                   //Diigo; a social bookmarking and annotation tool
+        public static readonly string ui_service_email = "email";                   //The AddThis email interface
+        public static readonly string ui_service_facebook = "facebook";             //Facebook; a social network
+        public static readonly string ui_service_fark = "fark";                     //Fark; a news aggregator
+        public static readonly string ui_service_faves = "faves";                   //Faves; a social bookmarking tool
+        public static readonly string ui_service_favorites = "favorites";           //Adds the page to the browser's bookmarks
+        public static readonly string ui_service_friendfeed = "friendfeed";         //Friendfeed; a micro-blogging platform
+        public static readonly string ui_service_google = "google";                 //Google Bookmarks; a bookmarking tool
+        public static readonly string ui_service_kaboodle = "kaboodle";             //Kaboodle; a social shopping tool
+        public static readonly string ui_service_kirtsy = "kirtsy";                 //Kirtsy; a social news aggregator
+        public static readonly string ui_service_linkagogo = "linkagogo";           //LinkaGoGo; a social bookmarking tool
+        public static readonly string ui_service_linkedin = "linkedin";             //LinkedIn; a social network
+        public static readonly string ui_service_live = "live";                     //Microsoft Live; a media sharing tool
+        public static readonly string ui_service_misterwong = "misterwong";         //Mister Wong; a social bookmarking tool
+        public static readonly string ui_service_mixx = "mixx";                     //Mixx; a social bookmarking tool
+        public static readonly string ui_service_multiply = "multiply";             //Multiply; a media sharing tool
+        public static readonly string ui_service_myaol = "myaol";                   //MyAOL; a start page
+        public static readonly string ui_service_myspace = "myspace";               //MySpace; a social network
+        public static readonly string ui_service_netvibes = "netvibes";             //Netvibes; a start page
+        public static readonly string ui_service_netvouz = "netvouz";               //Netvouz; a social bookmarking tool
+        public static readonly string ui_service_newsvine = "newsvine";             //Newsvine; a social news aggregator
+        public static readonly string ui_service_plaxo = "plaxo";                   //Plaxo; a social network
+        public static readonly string ui_service_propeller = "propeller";           //Propeller; a social news aggregator
+        public static readonly string ui_service_print = "print";                   //Prints the page
+        public static readonly string ui_service_reddit = "reddit";                 //Reddit; a social news aggregator
+        public static readonly string ui_service_simpy = "simpy";                   //Simpy; a social bookmarking tool
+        public static readonly string ui_service_slashdot = "slashdot";             //Slashdot; a social technology news aggregator
+        public static readonly string ui_service_spurl = "spurl";                   //Spurl; a social bookmarking tool
+        public static readonly string ui_service_stumbleupon = "stumbleupon";       //StumbleUpon; a content discovery tool
+        public static readonly string ui_service_stylehive = "stylehive";           //Stylehive; a fashion-focused social bookmarking tool
+        public static readonly string ui_service_technorati = "technorati";         //Technorati; a blog indexing and tracking tool
+        public static readonly string ui_service_thisnext = "thisnext";             //ThisNext; a social shopping tool
+        public static readonly string ui_service_tipd = "tipd";                     //Tip'd; a financial news aggregator
+        public static readonly string ui_service_twitter = "twitter";               //Twitter; a micro-blogging platform
+        public static readonly string ui_service_tumblr = "tumblr";                 //Tumblr; a blogging platform
+        public static readonly string ui_service_typepad = "typepad";               //TypePad; a blogging platform
+        public static readonly string ui_service_wordpress = "wordpress";           //WordPress; a blogging platform
+        public static readonly string ui_service_yahoobkm = "yahoobkm";             //Yahoo! Bookmarks; a bookmarking tool
+        public static readonly string ui_service_yardbarker = "yardbarker";         //Yardbarker; a social sports news aggregator
+        public static readonly string ui_service_meneame = "meneame";               //Menéame; 
         #endregion
     }
 }
