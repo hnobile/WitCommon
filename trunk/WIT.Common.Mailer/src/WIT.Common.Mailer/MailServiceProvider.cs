06edfbea49ea96f8d8f091e91354225aee998088
paliscioni <paliscioni@e84052d8-b9b0-0245-9862-b72e0d739b76>
2009-12-10 20:36:38 +0000

    WIT.Common.Mailer

---
+++ trunk/WIT.Common.Mailer/src/WIT.Common.Mailer/MailServiceProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using WIT.Common.ReflectionHelper;
+
+namespace WIT.Common.Mailer
+{
+    public class MailServiceProvider
+    {
+        private static IMailService _instance = null;
+
+        public IMailService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    string typeName = ConfigurationManager.AppSettings[WellKnownKeys.MailService_Implementation.ToString()];
+
+                    _instance = ReflectionHelper.ReflectionHelper.GetInstance<IMailService>(typeName);
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
