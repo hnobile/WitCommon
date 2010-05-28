7af8eb6396d09b643338a8333a3ce45892bc6215
paliscioni <paliscioni@e84052d8-b9b0-0245-9862-b72e0d739b76>
2010-05-28 16:04:28 +0000

    Exception Handling improvement.

--- WIT.Common.ReflectionHelper/src/WIT.Common.ReflectionHelper/ReflectionHelper.cs
+++ WIT.Common.ReflectionHelper/src/WIT.Common.ReflectionHelper/ReflectionHelper.cs
@@ -7,20 +7,28 @@ namespace WIT.Common.ReflectionHelper
     /// </summary>
     public class ReflectionHelper
     {
-        #region Public Methods
-
         /// <summary>
         /// Creates an instance of a given type specified through it's name.
         /// </summary>
         /// <typeparam name="T">Interface implemented by the specified type.</typeparam>
         /// <param name="TypeName">Type to instance.</param>
         /// <returns>Returns a new instance of the specified type, cast to the given interface.</returns>
+        /// <exception cref="ReflectionException">An invalid type name was given.</exception>
         /// <exception cref="ReflectionException">The specified type does not implement the given interface.</exception>
+        /// <exception cref="ReflectionException">An instance could not be created for the given type.</exception>
         public static T GetInstance<T>(string TypeName)
         {
             Type Interface = typeof(T);
-            
-            Type ReflectedType = Type.GetType(TypeName);
+
+            Type ReflectedType = null;
+            try
+            {
+                ReflectedType = Type.GetType(TypeName);
+            }
+            catch (Exception ex)
+            {
+                throw new ReflectionException("Invalid type name:" + TypeName, ex);
+            }
 
             bool ImplementsInterface = false;
             foreach (Type t in ReflectedType.GetInterfaces())
@@ -34,12 +42,17 @@ namespace WIT.Common.ReflectionHelper
 
             if (!ImplementsInterface)
             {
-                throw new ReflectionException("The given type does not implement the given interface.");
+                throw new ReflectionException("The given type does not implement the given interface");
             }
 
-            return (T)Activator.CreateInstance(ReflectedType);
+            try
+            {
+                return (T)Activator.CreateInstance(ReflectedType);
+            }
+            catch (Exception ex)
+            {
+                throw new ReflectionException("Could not create instance for type: " + TypeName, ex);
+            }
         }
-
-        #endregion Public Methods
     }
 }
