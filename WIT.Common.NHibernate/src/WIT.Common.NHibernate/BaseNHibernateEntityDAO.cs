6bef6d28aba73975ee72a58801567b7fbb4f61df
ilopez <ilopez@e84052d8-b9b0-0245-9862-b72e0d739b76>
2011-08-11 18:19:21 +0000

    Se actualizó la versión de NHibernate a la 3.2.0

--- WIT.Common.NHibernate/src/WIT.Common.NHibernate/BaseNHibernateEntityDAO.cs
+++ WIT.Common.NHibernate/src/WIT.Common.NHibernate/BaseNHibernateEntityDAO.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NHibernate;
-using NHibernate.Expression;
+using NHibernate.Criterion;
 
 namespace WIT.Common.NHibernate
 {
--- WIT.Common.NHibernate/src/WIT.Common.NHibernate/Properties/AssemblyInfo.cs
+++ WIT.Common.NHibernate/src/WIT.Common.NHibernate/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WIT.Common.NHibernate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Wormhole IT")]
 [assembly: AssemblyProduct("WIT.Common.NHibernate")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2009")]
+[assembly: AssemblyCopyright("Copyright © WormholeIT 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
