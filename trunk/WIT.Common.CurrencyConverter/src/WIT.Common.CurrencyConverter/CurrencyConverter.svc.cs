4d2f933a7aefb9b6a05d2845ed0f088023ba86e5
sportnoi <sportnoi@e84052d8-b9b0-0245-9862-b72e0d739b76>
2013-03-19 15:39:09 +0000

    WMB-8992: Crear un Servicio WIT.Common.CurrencyConverter que al pasarle un código de currency devuelva la cotización del dolar correspondiente.

---
+++ trunk/WIT.Common.CurrencyConverter/src/WIT.Common.CurrencyConverter/CurrencyConverter.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WIT.Common.CurrencyConverter
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    public class CurrencyConverter
+    {
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+
+        [WebGet(UriTemplate = "/API/GetCurrencyConversionRate?currencyCode={currencyCode}")]
+        [OperationContract]
+        public float GetCurrencyConversionRate(string currencyCode)
+        {
+            float conversionRate = 1;
+
+
+            return conversionRate;
+        }
+    }
+}
