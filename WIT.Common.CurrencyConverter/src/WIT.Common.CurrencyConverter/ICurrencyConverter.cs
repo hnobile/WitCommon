47fa55bbe21fa3405fcc5923dcee76440b8b074b
sportnoi <sportnoi@e84052d8-b9b0-0245-9862-b72e0d739b76>
2013-03-19 17:30:08 +0000

    WMB-8992: Crear un Servicio WIT.Common.CurrencyConverter que al pasarle un código de currency devuelva la cotización del dolar correspondiente.

---
+++ WIT.Common.CurrencyConverter/src/WIT.Common.CurrencyConverter/ICurrencyConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WIT.Common.CurrencyConverter
+{
+    [ServiceContract]
+    public interface ICurrencyConverter
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetCurrencyConversionRate?currencyCode={currencyCode}")]
+        float GetCurrencyConversionRate(string currencyCode);
+    }
+}
