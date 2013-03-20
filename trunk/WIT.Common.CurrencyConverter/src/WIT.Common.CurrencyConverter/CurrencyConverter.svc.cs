ee64818fe06d23cd989e254de2956fe2760082e7
abandeira <abandeira@e84052d8-b9b0-0245-9862-b72e0d739b76>
2013-03-20 14:39:00 +0000

    WWWC ISSUE WMB-8992 - Crear un Servicio WIT.Common.CurrencyConverter que al pasarle un código de currency devuelva la cotización del dolar correspondiente.

--- trunk/WIT.Common.CurrencyConverter/src/WIT.Common.CurrencyConverter/CurrencyConverter.svc.cs
+++ trunk/WIT.Common.CurrencyConverter/src/WIT.Common.CurrencyConverter/CurrencyConverter.svc.cs
@@ -23,9 +23,9 @@ namespace WIT.Common.CurrencyConverter
             float rate = -1;
             if (!string.IsNullOrEmpty(currencyCode))
             {
-                var search = (from c in currencies.Elements("currency")
-                                    where c.Attribute("code").Value == currencyCode
-                                    select c.Element("rate").Value);
+                var search =    from c in currencies.Elements("currency")
+                                where c.Attribute("code").Value == currencyCode
+                                select c.Element("rate").Value;
                 if (search.Count() > 0)
                 {
                     rate = float.Parse(search.First());
